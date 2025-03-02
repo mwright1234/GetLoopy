18548464fde712dc40c1d364a0d31c4709076167
Marco Dempsey Wright <wrigh4md@mail.uc.edu>
2025-03-02 17:31:14 -0500

    second

--- src/task2.java
+++ src/task2.java
@@ -1,2 +1,14 @@
 public class task2 {
+    public static void main(String[] args){
+
+        for (int i = 30; i >= 0; i--){
+
+            System.out.print(i + " ");
+        }
+
+
+
+    }
+
+
 }
--- src/task4.java
+++ src/task4.java
@@ -1,2 +1,13 @@
 public class task4 {
+    public static void main(String[] args){
+
+        for (int i = 10; i >=0 ; i -= 2) {
+            System.out.print(i + " ");
+
+
+        }
+
+
+
+    }
 }
